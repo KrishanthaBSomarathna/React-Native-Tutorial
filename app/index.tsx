2e5f1293d2ec050bf15cd25db6748f4f7483f6ba
Krishantha Bandara <info.krishantha.b@gmail.com>
2025-04-09 00:26:21 +0530

    view

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,9 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    "Helloworl"
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "black" }}>
+<View style={{ width: 200, height: 200, backgroundColor: "red" }}></View>
+<View style={{ width: 200, height: 200, backgroundColor: "green" }}></View>
+    </View>
   );
 }
