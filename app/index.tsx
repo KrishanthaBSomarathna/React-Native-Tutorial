3f55954d5a7fdf76af1ce6d6fa02d34fa6adb200
Krishantha Bandara <info.krishantha.b@gmail.com>
2025-04-09 01:16:17 +0530

    image

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,16 @@
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
+const img = require("../assets/images/partial-react-logo.png");
 
 export default function Index() {
   return (
-    "Helloworl"
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Image source={img} style={{flex: 2, width: 200, height: 200 }} />
+    </View>
   );
 }
