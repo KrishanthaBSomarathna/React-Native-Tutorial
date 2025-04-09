df17c0d864ac8d7d6d5d2ab655fbf1695cd74bc0
Krishantha Bandara <info.krishantha.b@gmail.com>
2025-04-09 22:11:56 +0530

    scrollView

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,34 @@
-import { Text, View, Image } from "react-native";
+import React from "react";
+import { Text, View, Image, ScrollView, StyleSheet } from "react-native";
+
 const img = require("../assets/images/partial-react-logo.png");
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Image source={img} style={{flex: 2, width: 200, height: 200 }} />
-    </View>
+    <ScrollView contentContainerStyle={[styles.container]}>
+      <Image source={img} style={styles.image} />
+      <Text style={styles.text}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+      </Text>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+    marginBottom: 16,
+  },
+  text: {
+    fontSize: 16,
+    textAlign: "center",
+  },
+});
